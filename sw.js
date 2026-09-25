e8fb5449f38a6364f8706bea617e2a68688fcf4b
Rogerogeaz <rogerogeaz@gmail.com>
2026-09-25 15:34:41 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,59 @@
+const CACHE_NAME = "tops-chicken-customer-v1";
+
+const APP_FILES = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(APP_FILES);
+      })
+      .then(function() {
+        return self.skipWaiting();
+      })
+  );
+});
+
+self.addEventListener("activate", function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames
+          .filter(function(name) {
+            return name !== CACHE_NAME;
+          })
+          .map(function(name) {
+            return caches.delete(name);
+          })
+      );
+    }).then(function() {
+      return self.clients.claim();
+    })
+  );
+});
+
+self.addEventListener("fetch", function(event) {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    caches.match(event.request).then(function(cachedResponse) {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+
+      return fetch(event.request)
+        .then(function(networkResponse) {
+          return networkResponse;
+        })
+        .catch(function() {
+          return caches.match("./index.html");
+        });
+    })
+  );
+});
